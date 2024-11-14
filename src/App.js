2985e7b43ef1c9344ce1317e5467d3aa0b492b54
Artem-Borodich <Nometa847@gmail.com>
2024-11-15 02:02:44 +0300

    минус ненужный компонент Dost

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import React, { useState } from 'react';
 import SearchBar from './components/SearchBar';
 import MapComponent from './components/MapComponent';
-import Dost from "./components/Dost";
 
 const App = () => {
   const [searchLocation, setSearchLocation] = useState(null);
@@ -16,7 +15,7 @@ const App = () => {
       <h1>City Guide</h1> {/* Проверьте, отображается ли заголовок */}
       <SearchBar onSearch={handleSearch} />
       <MapComponent searchLocation={searchLocation} />
-      <Dost />
+    
     </div>
   );
 };
